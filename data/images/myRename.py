01274f8421c6818af47783b3302f0c76d7359df5
zhangxiaoya <tiyijushi@163.com>
2015-04-22 15:35:39 +0800

    change data path

---
+++ data/images/myRename.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+def rename():
+    path = raw_input("Please input the path name ")
+    print path
+    forMat = raw_input("Please input the extention ")
+    print forMat
+    for(path,dirs,files) in os.walk(path):
+        for filename in files:
+            ext = os.path.splitext(filename)[1]
+            name = os.path.splitext(filename)[0]
+            if(ext == forMat):
+                print filename
+                print name
+                #name_part = name.replace("frame_","")
+                newname = str(int(name)) + forMat
+                oldpath = path + "//" + filename
+                newpath = path + "//" + newname
+                try:
+                    os.rename(oldpath,newpath)
+                except BaseException, e:
+                    print(str(e))
+                print newpath
+
+if __name__ == '__main__':
+    rename()
+
