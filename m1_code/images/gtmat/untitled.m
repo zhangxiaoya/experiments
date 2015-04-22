90c70c6c12f7940f8f9bc589dde8b978cb203640
zhangxiaoya <tiyijushi@163.com>
2015-04-22 15:49:49 +0800

    delete images dir in code dir

--- m1_code/images/gtmat/untitled.m
+++
@@ -1,8 +0,0 @@
-
-
-
-gtCenters = zeros(size(groundtruth_rect,1),2);
-for i = 1:size(groundtruth_rect,1)
-    gtCenters(i,1) = groundtruth_rect(i,1) + groundtruth_rect(i,3)/2;
-    gtCenters(i,2) = groundtruth_rect(i,2) + groundtruth_rect(i,4)/2;
-end
